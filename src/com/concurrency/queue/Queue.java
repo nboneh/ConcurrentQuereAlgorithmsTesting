f9e5680c24235d2029c676b0267993224281c068
Nir Boneh <nboneh5921@gmail.com>
2016-03-29 18:34:38 -0600

    Finished heap queue

--- src/com/concurrency/queue/Queue.java
+++ src/com/concurrency/queue/Queue.java
@@ -1,16 +1,6 @@
 package com.concurrency.queue;
 
 public interface  Queue<T> {
-	public class Node<T> {
-		 T value;
-		 int score;
-		 
-		 Node(T value, int score){
-			this.value = value;
-			this.score = score;
-			
-		 }
-	}
 	
 	 public void add(T element, int score);
 
--- src/com/concurrency/queue/SequentialQueue.java
+++ src/com/concurrency/queue/SequentialQueue.java
@@ -4,25 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SequentialQueue<T> implements Queue<T> {
+	private static class Node<T> {
+		 T value;
+		 int score;
+		 
+		 Node(T value, int score){
+			this.value = value;
+			this.score = score;
+			
+		 }
+	}
 	//Just a basic squential list queue implementation
 	private List<Node<T>> queue;
-	
+
 	public SequentialQueue(){
 		queue = new ArrayList<Node<T>>();
 	}
 	@Override
 	public void add(T element, int score) {
-		synchronized(this){
 		queue.add(new Node<T>(element, score));
-		}
 	}
 
 	@Override
 	public T removeMin() throws EmptyQueueException{
-		synchronized(this){
 		if(queue.isEmpty())
 			throw new EmptyQueueException();
-		
+
 		int index = 0;
 		int lowestScore = Integer.MAX_VALUE;
 		for(int i = 0; i < queue.size(); i++){
@@ -32,11 +39,10 @@ public class SequentialQueue<T> implements Queue<T> {
 				lowestScore = node.score;
 			}
 		}
-		
+
 		Node<T> node = queue.get(index);
 		queue.remove(index);
 		return node.value;
-		}
 	}
 
 }
